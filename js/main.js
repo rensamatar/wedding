35d83a46e941bb3a75d5f06fdc2aea709cc42b75
Tar <tar@iwa.fi>
2020-02-24 13:37:30 +0700

    Updated coundown timer

--- js/main.js
+++ js/main.js
@@ -221,15 +221,19 @@
 		var now = new Date().getTime();
 
 		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
+		var distance = now - countDownDate;
 
 		// Time calculations for days, hours, minutes and seconds
 		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		var years = Math.floor(days / 365);
+		var months = Math.floor(years / 31);
 
 		// Display the result in an element with id="demo"
+		document.getElementById("years").innerHTML = years +" <small>years</small>";
+		document.getElementById("months").innerHTML = months +" <small>months</small>";
 		document.getElementById("days").innerHTML = days +" <small>days</small>";
 		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
 		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
